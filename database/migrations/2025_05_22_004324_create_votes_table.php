7ade41da1c67320d63619ea28c909383c5c547ac
Bubhux <bubhuxpaindepice@gmail.com>
2025-05-24 10:36:08 +0200

    Mise à jour V.1.9  Changes to be committed: 	renamed:    database/migrations/2025_05_22_015314_create_propositions_table.php -> database/migrations/2025_05_21_015314_create_propositions_table.php 	modified:   database/migrations/2025_05_22_004324_create_votes_table.php 	modified:   resources/views/people/show.blade.php

--- database/migrations/2025_05_22_004324_create_votes_table.php
+++ database/migrations/2025_05_22_004324_create_votes_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('votes');
+        Schema::dropIfExists('modification_votes');
     }
 };
